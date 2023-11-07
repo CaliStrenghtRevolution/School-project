672cd64d2170c342a40316df80d565017f9f79aa
Endles13 <144099247+Endles13@users.noreply.github.com>
2023-11-07 09:46:25 +0100

    Final.
    This is the final version. Will we succeed? Who knows.

--- Vaja1/homelol/upload.php
+++ Vaja1/homelol/upload.php
@@ -24,16 +24,48 @@ if (isset($_SESSION["Name"])) {
                 $icon_upload_path = 'student-files/' . $new_icon_name;
                 move_uploaded_file($icon_tmp_name, $icon_upload_path);
 
-                $db = mysqli_connect(Localhost, Username, Password, Name);
-                $date = date("Y-m-d");
-                $id_student = $_SESSION["id"];
-                $id_materials = $_POST['variable_to_pass'];
+                $SQL1 = "SELECT file FROM submission WHERE id_student = ".$_SESSION["id"]."";
+                $querry = $db -> prepare($SQL1);
+                $querry -> execute();
+                $result = $querry -> get_result();
+                if(mysqli_num_rows($result) != 0)
+                {
+                    $user = $result->fetch_assoc();
+    
+                    $_file= $user['file'];
+                    $db -> close();
+                }
 
-                $SQL = "INSERT INTO submission (File, id_student, id_materials) VALUES (?, ?, ?)";
-                $querry = $db->prepare($SQL);
-                $querry->bind_param("sss", $new_icon_name, $id_student, $id_materials);
-                $querry->execute();
-                $db->close();
+                if($_file === $new_icon_name)
+                {
+                    $change_id = $_SESSION["id"];
+                    $change = $new_icon_name;
+                    $sql = "UPDATE submission SET file = ? WHERE id_student = $change_id";
+                    $db = mysqli_connect(Localhost, Username, Password, Name);
+                    if (!$db) {
+                        die("Connection error: " . mysqli_connect_error());
+                    }
+                    $stmt = $db->prepare($sql);
+                    $stmt->bind_param("s", $change);
+                    if ($stmt->execute())
+                    {
+                        
+                    }
+                    $stmt->close();
+                }
+                else
+                {
+                    $db = mysqli_connect(Localhost, Username, Password, Name);
+                    $date = date("Y-m-d");
+                    $id_student = $_SESSION["id"];
+                    $id_materials = $_POST['variable_to_pass'];
+    
+                    $SQL = "INSERT INTO submission (File, id_student, id_materials) VALUES (?, ?, ?)";
+                    $querry = $db->prepare($SQL);
+                    $querry->bind_param("sss", $new_icon_name, $id_student, $id_materials);
+                    $querry->execute();
+                    $db->close();
+                }
             } else {
                 $em = "You can't upload files of this type!";
                 header("Location: index.php?error=$em");
